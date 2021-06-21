81d0834bb0ab98ac553c4340e1a976ae79b3873b
MSP-Greg <Greg.mpls@gmail.com>
2021-06-21 18:49:02 -0500

    pacman_query.rb - add ucrt

--- working_directory/pacman_query.rb
+++ working_directory/pacman_query.rb
@@ -30,33 +30,53 @@ module Msys2Info
         puts "", "File dir: #{__dir__}", ""
       end
 
-      a_i686  = `pacman.exe -Q | grep ^mingw-w64-i686- | sed 's/^mingw-w64-i686-//'`.split(/\r?\n/)
-      a_x64   = `pacman.exe -Q | grep ^mingw-w64-x86_64- | sed 's/^mingw-w64-x86_64-//'`.split(/\r?\n/)
+      all_pkgs = `pacman.exe -Q`.split(/\r?\n/)
+      a_ucrt = find_pkgs(all_pkgs, 'mingw-w64-ucrt-x86_64-')
+      a_x64  = find_pkgs(all_pkgs, 'mingw-w64-x86_64-')
+      a_i686 = find_pkgs(all_pkgs, 'mingw-w64-i686-')
 
-      unless a_i686.empty? && a_x64.empty?
-        highlight "#{(dash + ' mingw-w64-x86_64 Packages ' + dash).ljust WID} #{dash} mingw-w64-i686 Packages #{dash}"
-        max_len = [a_i686.length, a_x64.length].max - 1
-        j, k = 0,0
-        0.upto(max_len) { |i|
+      unless a_ucrt.empty? && a_x64.empty? && a_i686.empty?
+        highlight "#{(dash[0..-3] + ' mingw-w64-ucrt-x86_64 Packages ' + dash[0..-3]).ljust WID} #{(dash + ' mingw-w64-x86_64 Packages ' + dash).ljust WID} #{dash} mingw-w64-i686 Packages #{dash}"
+
+        j_len = a_ucrt.length
+        k_len = a_x64.length
+        l_len = a_i686.length
+
+        j, k, l = 0, 0, 0
+        while j < j_len &&  k < k_len && l < l_len do
           # get package base name
-          x64  = a_x64[j]
-          i686 = a_i686[k]
-          if x64 != i686
-            if x64 < i686
-              puts x64
-              j += 1
-            elsif i686 < x64
-              puts "#{''.ljust WID} #{i686}"
-              k += 1
-            end
+          ucrt = a_ucrt[j]
+          x64  = a_x64[k]
+          i686 = a_i686[l]
+
+          b_ucrt = ucrt[/\S+/]
+          b_x64  = x64[/\S+/]
+          b_i686 = i686[/\S+/]
+
+          b_min = [b_ucrt, b_x64, b_i686].min { |a, b| a.downcase <=> b.downcase }
+
+          out = ''.dup
+          if b_min == b_ucrt
+            j += 1
+            out << "#{ucrt.ljust WID} "
           else
-            puts "#{x64.ljust WID} #{i686}"
-            j += 1 ; k += 1
+            out << "#{' '.ljust WID} "
           end
-        }
+          if b_min == b_x64
+            k += 1
+            out << "#{x64.ljust WID} "
+          else
+            out << "#{' '.ljust WID} "
+          end
+          if b_min == b_i686
+            l += 1
+            out << i686
+          end
+          puts out.rstrip
+        end
       end
 
-      a_msys2 = `pacman.exe -Q | grep -v ^mingw-w64-`.split(/\r?\n/)
+      a_msys2 = all_pkgs.reject { |str| str.start_with? 'mingw-w64-' }.sort_by(&:downcase)
 
       unless a_msys2.empty?
         puts ''
@@ -66,6 +86,10 @@ module Msys2Info
           puts "#{(a_msys2[i] || '').ljust WID} #{a_msys2[i + half] || ''}"
         }
       end
+    end
+
+    def find_pkgs(ary, query)
+      ary.select { |str| str.start_with? query }.map { |str| str.sub query, '' }.sort_by(&:downcase)
     end
 
     def highlight(str)
